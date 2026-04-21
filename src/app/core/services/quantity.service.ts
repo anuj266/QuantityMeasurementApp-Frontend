38d327c0f202cdea11a1ad2de56810dab5d723dc
anuj266 <anujudaywal@gmail.com>
2026-04-21 11:23:05 +0530

    [Anuj Udaywal] Refactor code

--- src/app/core/services/quantity.service.ts
+++ src/app/core/services/quantity.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+
 
 export interface QuantityDTO {
   value: number;
